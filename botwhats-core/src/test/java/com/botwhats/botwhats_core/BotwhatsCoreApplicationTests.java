d6c2e2a96fc217190b16591870868968d3c01705
Cesar Henrique <ceh.paiva@gmail.com>
2026-02-16 19:12:25 -0300

    Adicionado itens do Initializr

---
+++ botwhats-core/src/test/java/com/botwhats/botwhats_core/BotwhatsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botwhats.botwhats_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotwhatsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
