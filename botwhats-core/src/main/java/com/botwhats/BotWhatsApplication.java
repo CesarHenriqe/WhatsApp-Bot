f4f713d7aa4d88a204ffb4bebb2f8026f87f1736
Cesar Henrique <ceh.paiva@gmail.com>
2026-02-17 19:52:53 -0300

    Refatora projeto subindo com banco OK

---
+++ botwhats-core/src/main/java/com/botwhats/BotWhatsApplication.java
@@ -0,0 +1,12 @@
+package com.botwhats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotWhatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotWhatsApplication.class, args);
+    }
+}
---
+++ botwhats-core/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test_health (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
